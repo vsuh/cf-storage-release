ce6f54511cac8be213577f99745b3d337bf03005
vsuh <vsuh@mail.ru>
2021-12-03 08:51:27 +0300

    resort files in folders

--- cmd/bat/2.cmd
+++ cmd/bat/2.cmd
@@ -1,6 +1,6 @@
 :: Попытка выгрузить релиз расширения №2
 :: по мотивам дисуссии в https://infostart.ru/journal/news/mir-1s/ne-zabyvayte-o-vozmozhnosti-sozdat-khranilishche-konfiguratsii-dlya-rasshireniy_952832/
-
+:: не работает
 Set "Version=8.3.18.1483"
 Set exe1c="C:\Program Files (x86)\1cv8\%Version%\bin\1cv8.exe"
 Set "ib=v8r_TempDB"
